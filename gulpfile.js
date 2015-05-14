8216fb5355691118e9ce85db9ef35bb2bd7f92ed
John Babak <babak.john@gmail.com>
2015-05-14 03:45:26 +0300

    Reworked the build sequence, fixed bugs
    * Fixed build failing for the first time (if there are no compiled
    files).
    * Added package.json for the app (app name, window options, build
    options: configurable NW.js version and output platforms).
    * Added production build (without the toolbar).
    * Removed the stub texts in the HTML and the app metadata.
    * Cleaned up the tasks, removed the older paths.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,33 @@
 var gulp = require('gulp');
 var requireDir = require('require-dir');
+var runSequence = require('run-sequence');
 
 requireDir('./gulp');
 
 gulp.task('watch-compile', ['compile'], function() {
   gulp.watch('./src/**/*', ['browserify']);
-  gulp.watch(['./index.html', './index.js'], ['through']);
-  gulp.watch('./style/**/*', ['less']);
+  gulp.watch('./src/index.html', ['through']);
+  gulp.watch('./src/style/**/*', ['less']);
 });
 
-gulp.task('watch-build', ['build'], function() {
-  gulp.watch('./compile/**/*', ['build']);
+gulp.task('watch-build', ['nw'], function() {
+  gulp.watch('./compile/**/*', ['nw']);
 });
 
-gulp.task('default', ['watch-compile', 'watch-build']);
+gulp.task('watch', function(done) {
+  runSequence(
+    'watch-compile',
+    'watch-build',
+    done
+  );
+});
+
+gulp.task('build', function(done) {
+  runSequence(
+    'compile',
+    'nw',
+    done
+  );
+});
+
+gulp.task('default', ['watch']);
